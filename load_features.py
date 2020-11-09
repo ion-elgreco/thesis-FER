bf095a5a6d7a00f6e3270ef5cb05cbe91ab5dac7
ion-elgreco <ioncjk@gmail.com>
2020-11-09 19:35:49 +0100

    Added test feature extraction for AFF-Wild2

--- load_features.py
+++ load_features.py
@@ -5,28 +5,25 @@ train_features = scipy.sparse.load_npz("data/train_features.npz") #CSR Matrix
 val_features = scipy.sparse.load_npz("data/val_features.npz") #CSR Matrix
 train_labels = np.load("data/train_labels.npy") #Numpy array
 val_labels = np.load("data/val_labels.npy") #Numpy array
+test_features = scipy.sparse.load_npz("data/test_features.npz") #CSR Matrix
 
 # Function which reshapes the features to [sequences, sequence_length, features], 
 # and labels to [sequences, sequence_length, labels]
-def reshaper(features, labels, sequence_length):
-    """
-    :param features: extracted image features
-    :type features: scipy CSR matrix (2-Dim)
-    :param labels: one-hot encoded labels of each image
-    :type labels: numpy array (2-Dim)
-    :param sequence_length: the length of the sequence you want. E.g. You want 25 images in a seqeunce so, value is 25
-    :type sequence_length: int
-    
-    """
+def labels_reshaper(labels, sequence_length):
     # Find the amount of possible sequences with given sequence_length. Some data will be discarded this way.
-    amount = features.shape[0] // sequence_length
+    amount = labels.shape[0] // sequence_length
     
-    # Convert Scipy matrix back to numpy array because we need to convert it to 3 Dimensional array
+    # Reshapes the labels
+    seq_labels = np.reshape(labels[:(amount * sequence_length)], (amount, sequence_length, labels.shape[1]))
+
+    return seq_labels
+
+# Function which reshapes the features to [sequences, sequence_length, features],
+def features_reshaper(features, sequence_length):
+    amount = features.shape[0] // sequence_length
     arr_features = features[:(amount * sequence_length)].toarray()
     
-    # Reshapes the labels and features
+    # Reshapes the features
     seq_features = np.reshape(arr_features, (amount, sequence_length, arr_features.shape[1]))
-    seq_labels = np.reshape(labels[:(amount * sequence_length)], (amount, sequence_length, labels.shape[1]))
     
-    
-    return seq_features, seq_labels
+    return seq_features
