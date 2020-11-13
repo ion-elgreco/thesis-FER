8d0a6b784d99ddff456a391bc71def2aebbc5df8
ion-elgreco <ioncjk@gmail.com>
2020-11-13 20:35:49 +0100

    Removed F1-custom metric, I need to create F1-callback instead

--- functions.py
+++ functions.py
@@ -3,15 +3,6 @@ import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
 
 # Source: https://github.com/keras-team/keras/blob/1c630c3e3c8969b40a47d07b9f2edda50ec69720/keras/metrics.py
-def f1(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    recall = true_positives / (possible_positives + K.epsilon())
-    f1_val = 2 * (precision * recall) / (precision + recall + K.epsilon())
-    return f1_val
-
 def plot_history(data_list, label_list, title, ylabel):
 
     epochs = range(1, len(data_list[0]) + 1)
